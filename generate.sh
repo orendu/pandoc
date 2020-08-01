6788c2b72d99d113fae65e30580243bf3106c4cd
orendu <oren@mellanox.com>
2020-08-01 20:03:36 +0300

    Create generate.sh

---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+IN="$1"
+OUT="$2"
+TITLE="$3"
+
+cd /github/workspace
+ver=`git describe --tags`
+echo VER=$ver
+
+unixtime=`git show -s --format=%ct`
+date=`date --date=@$unixtime +%F`
+echo DATE=$date
+
+sed "0,/\[VER\]/ s//$ver/g" "$IN" | sed "0,/\[DATE\]/ s//$date/g" | pandoc --from gfm --to html5 -N --css css/github-typora2.css --standalone -M title="$TITLE" -o "$OUT".pdf
