5149d5980086b93edd1150902d6c7dd7d6758199
orendu <oren@mellanox.com>
2020-08-01 23:46:11 +0300

    debug can't see css

--- generate.sh
+++ generate.sh
@@ -12,5 +12,7 @@ echo VER=$ver
 unixtime=`git show -s --format=%ct`
 date=`date --date=@$unixtime +%F`
 echo DATE=$date
-
+id
+ls -l /github/workspace/
+ls -l /github/workspace/css
 sed "0,/\[VER\]/ s//$ver/g" "$IN" | sed "0,/\[DATE\]/ s//$date/g" | pandoc --from gfm --to html5 -N --css /github/workspace/css/github-typora2.css --standalone -M title="$TITLE" -o "$OUT".pdf
