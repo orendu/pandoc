5a7117eb1ef859616d7685874a961e215e21e7c9
Oren Duer <oren@nvidia.com>
2020-08-02 10:55:42 +0300

    Upgraded to Pandoc 2.10
    Signed-off-by: Oren Duer <oren@nvidia.com>

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,14 @@
 FROM ubuntu:20.04
 
 COPY wkhtmltox_0.12.6-1.focal_amd64.deb /wkhtmltox_0.12.6-1.focal_amd64.deb
-COPY pandoc_2.5-3build2_amd64.deb /pandoc_2.5-3build2_amd64.deb
+COPY pandoc-2.10.1-1-amd64.deb /pandoc-2.10.1-1-amd64.deb
 COPY generate.sh /generate.sh
 RUN chmod a+x /generate.sh
 
 RUN apt-get update
 RUN apt-get install -y git
 RUN apt-get install -y /wkhtmltox_0.12.6-1.focal_amd64.deb
-RUN apt-get install -y /pandoc_2.5-3build2_amd64.deb
+RUN apt-get install -y /pandoc-2.10.1-1-amd64.deb
 
 #ENTRYPOINT ["/usr/bin/pandoc"]
 
--- generate.sh
+++ generate.sh
@@ -2,7 +2,8 @@
 
 IN="$1"
 OUT="$2"
-TITLE="$3"
+CSS="$3"
+TITLE="$4"
 
 cd /github/workspace
 git fetch --tags
@@ -12,7 +13,5 @@ echo VER=$ver
 unixtime=`git show -s --format=%ct`
 date=`date --date=@$unixtime +%F`
 echo DATE=$date
-id
-ls -l /github/workspace/
-ls -l /github/workspace/css
-sed "0,/\[VER\]/ s//$ver/g" "$IN" | sed "0,/\[DATE\]/ s//$date/g" | pandoc --from gfm --to html5 -N --css /github/workspace/css/github-typora2.css --standalone -M title="$TITLE" -o "$OUT".pdf
+
+sed "0,/\[VER\]/ s//$ver/g" "$IN" | sed "0,/\[DATE\]/ s//$date/g" | pandoc --from gfm --to html5 -N --css /github/workspace/"$CSS" --standalone -M title="$TITLE" -o "$OUT".pdf
